7e87808aeedc3f449b3ad8d0a73c4f04aace8000
Igor Stepanov <igorstepanov@hotmail.com>
2022-06-10 18:39:27 +0300

    build(Employees App): реализовать управляемые компоненты в форме добавления нового сотрудника

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,26 +1,51 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            onChange={this.onValueChange}
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="name"
+            value={name}
+          />
+          <input
+            onChange={this.onValueChange}
+            type="text"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            name="salary"
+            value={salary}
+          />
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
